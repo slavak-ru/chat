8fd4c19555ae1556ddaf014cb0bae609b6b80d09
Vyacheslav Knyazev <slavak-ru@yandex.ru>
2018-08-06 11:11:30 +0300

    install webpack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CHAT
 Educational project - Single Page Application on JavaScript components
+
+Demo: slavak-ru.github.io/chat/
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  entry: './dev/home',
+  output: {
+    path: __dirname,
+    filename: './production/build.js'
+  }
+};
