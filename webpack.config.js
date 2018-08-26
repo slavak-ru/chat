2d0851a4bd521c65642d2bed4032cbfb222d1487
Vyacheslav Knyazev <slavak-ru@yandex.ru>
2018-08-26 15:59:01 +0300

    configure webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,37 @@
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let pathsToClean = [
+  'dev',
+  'public'
+]
+
+function addHash(template, hash) {
+  return NODE_ENV == 'production' ?
+      template.replace(/\.[^.]+$/, `.[${hash}]$&`) : `${template}?hash=[${hash}]`;
+}
 
 module.exports = {
-  context: __dirname + '/dev',
+  context: __dirname + '/src',
+
+  mode: NODE_ENV,
 
   entry: {
-    home:  "./home",
-    about: "./about",
-    common: "./common"
+    index: './index',
   },
 
   output: {
-    path:     __dirname + '/production',
-    filename: "[name].js",
-    library:  "[name]"
+    path: path.resolve(NODE_ENV == 'production' ? __dirname + '/public' : __dirname + '/dev'),
+    publicPath: NODE_ENV == 'production' ? './' : '/',
+    filename: addHash('[name].js', 'hash:6'),
+    chunkFilename: '[id].js'
   },
 
   watch: NODE_ENV == 'development',
@@ -24,72 +41,152 @@ module.exports = {
     aggregateTimeout: 100
   },
 
-  devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : false,
+  devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : false,
 
-  optimization: {
-    noEmitOnErrors: true,
-    splitChunks: {
-      chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
-  },
-
-  plugins: [
-
+  plugins: [ 
+    new CleanWebpackPlugin(pathsToClean, {} ), // Clean 'dev' directory
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(NODE_ENV),
       LANG:     JSON.stringify('ru')
     }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: addHash('[name].css', 'hash:6'),
+      chunkFilename: addHash('[id].css', 'hash:6'),
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: false,
+      template: './chat.html',
+      filename: 'index.html',
+      title: 'CHAT'
+    })
   ],
 
   resolve: {
-    modulesDirectories: ['node_modules'],
-    extensions:         ['', '.js']
+    modules: ['node_modules'],
+    //modulesDirectories: ['node_modules'],
+    extensions: ['.js']
   },
 
   resolveLoader: {
-    modulesDirectories: ['node_modules'],
-    moduleTemplates:    ['*-loader', '*'],
-    extensions:         ['', '.js']
+    moduleExtensions: ['-loader'],
+    extensions: ['.js']
   },
 
   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                'targets': {
+                  // The % refers to the global coverage of users from browserslist
+                  'browsers': [ '>3%']
+                }
+              }]
+            ],
+            plugins: ['transform-runtime'],
+          }
+        }
+      },
+      {
+        test:   /\.pug$/,
+        use: { 
+          loader: 'pug-loader', 
+          query: {} // Can be empty
+        } 
+      },
+      {
+        test:/\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+                plugins: [
+                    autoprefixer({
+                        browsers:['ie >= 11', 'last 3 version']
+                    })
+                ],
+                sourceMap: true
+            }
+          },
+        ]  
+      },
+      {
+        test:/\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+                plugins: [
+                    autoprefixer({
+                        browsers:['ie >= 11', 'last 3 version']
+                    })
+                ],
+                sourceMap: true
+            }
+          },
+        ]  
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: addHash('file?name=[path][name].[ext]', 'hash:6'),
+              //outputPath: 'images/'
+            }
+          }
+        ]
+      }
+    ]
+  },
 
-    rules: [{
-      test:   /\.js$/,
-      loader: 'babel?optional[]=runtime'
-    }]
-
+  devServer: {
+    host: 'localhost', // default
+    port: 8080, // default
+    open: true,
+    //contentBase: path.join(__dirname, 'public'),
+    //hot: true
   }
-};
+
+}
 
 
+// Uglify
 if (NODE_ENV == 'production') {
   module.exports.plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
+    new UglifyJsPlugin({
+      uglifyOptions: {
         compress: {
           // don't show unreachable variables etc
           warnings:     false,
           drop_console: true,
           unsafe:       true
         }
-      })
-  );
+      }
+    })
+  )
 }
