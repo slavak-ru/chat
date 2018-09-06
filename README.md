9694d2e249156bed057691ad916dfccf0d9adfe9
Vyacheslav Knyazev <slavak-ru@yandex.ru>
2018-09-06 14:05:01 +0300

    merge master and webpack

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # CHAT
 Educational project - Single Page Application on JavaScript components
 
-Demo: slavak-ru.github.io/chat/
+Build system - webpack (#4.16.5).
+
+### Demo: 
+https://slavak-ru.github.io/chat/
+
+Install with npm:
+```bash
+npm install
+```
+
+Run in dev mode:
+```bash
+npm run chat:dev
+```
+
+Run in production mode:
+```bash
+npm run chat:prod
+```
---
+++ src/components/textarea/textarea.js
@@ -0,0 +1,35 @@
+
+/** @description Component Textarea define height of the textarea DOM-Element by adding/removing additional rows. */
+export default class Textarea {
+  /**
+		* @description define height of textarea.
+		* @param {object} element - the textarea (DOM-element).
+		* @param {number} cols - cols of the textarea.
+		* @param {number} rows - rows of the textarea.
+	*/
+  constructor({element}){
+    this.textarea = element;
+    this.cols = this.textarea.cols;
+    this.rows = this.textarea.rows;
+    this.maxValue = this.rows * this.cols;
+
+    this._textareaSize = this._textareaSize.bind(this);
+
+    this._initEvent();
+  }
+
+  _initEvent() {
+    this.textarea.addEventListener('keydown', this._textareaSize);
+  }
+
+  /**
+		* @method removeTooltip()
+		* @description Inner method - define textarea size (rows) by adding or removing additionalrow.
+	*/
+  _textareaSize() {
+    let curentValue = this.textarea.value.length;
+    let curentMaxValue = this.textarea.rows * this.textarea.cols;
+    if (curentValue > curentMaxValue - this.cols) this.textarea.rows = this.textarea.rows + 1;
+    if ( curentValue < curentMaxValue - (this.cols * 2) && this.textarea.rows  > this.rows )this.textarea.rows = this.textarea.rows - 1;
+  }
+}
